d52d0622d5e4b9da89ab1f655d2274048b5b9a63
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-06-07 02:30:29 +0800

    Update DP_CLCS.cpp

--- DP_CLCS.cpp
+++ DP_CLCS.cpp
@@ -12,6 +12,17 @@
 using namespace std;
 
 // ============================ 定义常量 ============================
+// 测试数据路径
+// 1000 * 1000; |bet| = 2;
+static string fileName1 = "runtime_1000_1000_2_2.csv";// m_n_k_alphabet 
+static string fileName2 = "string_1000_1000_2_2.csv";
+// 5000 * 5000; |bet| = 4;
+static string fileName3 = "runtime_5000_5000_2_4.csv";// m_n_k_alphabet 
+static string fileName4 = "string_5000_5000_2_4.csv";
+// 5000 * 5000; |bet| = 8;
+static string fileName5 = "runtime_5000_5000_2_8.csv";// m_n_k_alphabet 
+static string fileName6 = "string_5000_5000_2_8.csv";
+
 // alphabet size
 static int alphabet = 4;
 // LCSk in k length substring
@@ -25,17 +36,22 @@ static int r = 5;
 // upbound of run_time
 static int run_time = 100;
 
+// ============================  DP-CLCS by Chin ============================
+// Chin, F. Y., et al. (2004). "A simple algorithm for the constrained sequence problems." Information Processing Letters 90(4): 175-179.
+
+// 
 template<typename T>
 std::vector<std::vector<std::vector<T>>> make_3d_vector(int z, int y, int x, T value = T{}) {
     return std::vector<std::vector<std::vector<T>>>(z, std::vector<std::vector<T>>(y, std::vector<T>(x, value)));
 }
 
+
 /** Chin et al. algorithm */
 int DP_Chin(int* A, int* B, int* P, int m, int n, int r) {
-    // save in 3D vector 
+    // 0 save in 3D vector 
     auto M3d = make_3d_vector<int>(r + 1, m + 1, n + 1, 0);
 
-    // boundary conditions (initializations):
+    // 1 boundary conditions (initializations):
     // k = 0
     for (int i = 0; i < m + 1; i++) M3d[0][i][0] = 0;
     for (int j = 0; j < n + 1; j++) M3d[0][0][j] = 0;
@@ -47,25 +63,25 @@ int DP_Chin(int* A, int* B, int* P, int m, int n, int r) {
         for (int k = 1; k < r + 1; k++) M3d[k][0][j] = INT_MIN;
     }
 
-    // execute the DP recursion: 
+    // 2 execute the DP recursion: 
     for (int k = 0; k <= r; k++) {
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 int lmax = INT_MIN;
-                if (k > 0 && A[i - 1] == B[j - 1] && B[j - 1] == P[k - 1]) 
+                if (k > 0 && A[i - 1] == B[j - 1] && B[j - 1] == P[k - 1])
                     lmax = M3d[k - 1][i - 1][j - 1] + 1; // A[i] = B[j] = P[k]
-                else if (A[i - 1] == B[j - 1]) 
+                else if (A[i - 1] == B[j - 1])
                     lmax = M3d[k][i - 1][j - 1] + 1; // A[i] = B[j] != P[k] or k = 0
                 lmax = max(M3d[k][i - 1][j], lmax);
-                M3d[k][i][j] = max(M3d[k][i][j - 1], lmax);  
+                M3d[k][i][j] = max(M3d[k][i][j - 1], lmax);
             }
         }
     }
     // TODO: retrieving optimal solution
     int k = r, i = m, j = n, len = 0;
-    cout << "optimal solution " << M3d[ k ][ i ][ j ]  << " " << M3d[ 0 ][ 0 ][ 0 ] <<  endl;
-    
-    // Backtracing
+    cout << "optimal solution " << M3d[k][i][j] << " " << M3d[0][0][0] << endl;
+
+    // 3 Backtracing
     vector<int> s; // result
     while (!(i == 0 and j == 0 and k == 0))
     {
@@ -81,8 +97,9 @@ int DP_Chin(int* A, int* B, int* P, int m, int n, int r) {
         }
         else break;
     }
-    // reverse the vector to get the optimum
+    // reverse the vector to get the optimal sol
     std::reverse(s.begin(), s.end());
+    // cout << "s = "; for (int i : s) cout << i << ", "; cout << endl;
     return M3d[r][m][n];
 }
 
@@ -96,21 +113,8 @@ int main()
     for (int i = 0; i < n; i++) B[i] = rand() % alphabet + 1;
     for (int i = 0; i < r; i++) P[i] = rand() % alphabet + 1;
 
-
     int DP = DP_Chin(A, B, P, m, n, r);
     cout << "DP: " << DP << endl;
-
-    //int OURS_1 = Our_Method_1(A, B, m, n, k, alphabet);
-    //cout << "OURS_1: " << OURS_1 << endl;
-    //int OURS_2 = Our_Method_2_vector(A, B, m, n, k, alphabet);
-    //cout << "OURS_2: " << OURS_2 << endl;
-    //int OURS_3 = Our_Method_3(A, B, m, n, k, alphabet);
-    //cout << "OURS_3: " << OURS_3 << endl;
-    //int PV = PV_Method_All_array(A, B, m, n, k, alphabet);
-    //cout << "PV: " << PV << endl;
-    //int DG = DG_Method_Dense(A, B, m, n, k, alphabet);
-    //cout << "DG: " << DG << endl;
-
     system("pause");
     return 0;
 }
