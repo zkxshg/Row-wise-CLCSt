fa6fc7378e14fcd433551f1837a1f4234cc0ccb6
zkxshg <zkxshg@126.com>
2021-03-04 01:38:27 +0800

    Update LCS_DP.cpp

--- LCS_DP.cpp
+++ LCS_DP.cpp
@@ -36,7 +36,7 @@ int DP_LCS_Algorithm(int* A, int* B, int m, int n) {
         for (int j = 0; j < n; j++) Benson1[i][j] = 0;
     } 
 
-    // 2 boundary conditions
+    // 1 boundary conditions
     for (int i = 0; i < m; i++) {
         if (A[i] == B[0]) Benson1[i][0] = 1;
         else if (i > 0) Benson1[i][0] = Benson1[i - 1][0];
@@ -47,7 +47,7 @@ int DP_LCS_Algorithm(int* A, int* B, int m, int n) {
         else if (i > 0)  Benson1[0][i] = Benson1[0][i - 1];
     }
 
-    // 1.2 Use DP to calculate the LCS(i, j)
+    // 2 Use DP to calculate the LCS(i, j)
     for (int i = 1; i < m; i++) {
         for (int j = 1; j < n; j++) {
             Benson1[i][j] = max(Benson1[i][j - 1], Benson1[i - 1][j]);
