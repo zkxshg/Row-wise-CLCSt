a1be8c8a1fd82960beef1ff8725924ad546dafdb
zkxshg <zkxshg@126.com>
2022-05-13 19:25:33 +0800

    Add files via upload

---
+++ data/cut_seq.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <set>
+#include <climits>
+#include <unordered_map>
+#include<map> 
+// #include <windows.h>
+
+using namespace std;
+
+static string fileName = "D:/Lab/Tex-论文修改/7_Realdata/Expre Design/Exp1_diff_virus/Ref_COV19_EPI_ISL_406801.fasta";
+static string savefile = "D:/Lab/Tex-论文修改/7_Realdata/Expre Design/Exp1_diff_virus/A.fasta";
+
+// ==================== 主测试函数 ====================
+int main(int argc, char** argv) {
+    // int argc, char** argv
+    //// ========== = 读入参数 ========== =
+
+    fileName = argv[1];
+    savefile = argv[2];
+    int st = atoi(argv[3]), en = atoi(argv[4]);
+
+    ifstream fin(fileName);
+    string wholeS = "";
+
+    cout << "read file: " << fileName << endl;
+
+    string s;
+    while (fin >> s) wholeS += s;
+
+    cout << "cut subsequnce" << endl;
+    string sub = wholeS.substr(st, en - st);
+
+    cout << "The segment is: " << sub << endl;
+
+    ofstream outFile;
+    outFile.open(savefile, ios::out); 
+    outFile << sub;; outFile << endl;
+
+    outFile.close();
+
+    system("pause");
+    return 0;
+}
